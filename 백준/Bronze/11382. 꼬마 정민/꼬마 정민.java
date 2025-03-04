154137dab8fdc8b9bc3f1ffa80f2adc6e96dd054
Hyeonsu Son <101486154+soniloght@users.noreply.github.com>
2025-03-04 20:44:33 +0900

    [Bronze V] Title: 꼬마 정민, Time: 212 ms, Memory: 17620 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        long A, B, C;
+        A = sc.nextLong();
+        B = sc.nextLong();
+        C = sc.nextLong();
+
+        System.out.println(A + B + C);
+
+
+    }
+}
