0c5f3abc70ad1e5ab4a88f4946341248f85f14e8
Hyeonsu Son <101486154+soniloght@users.noreply.github.com>
2025-03-04 20:40:26 +0900

    [Bronze V] Title: 구구단, Time: 112 ms, Memory: 12936 KB -BaekjoonHub

---
+++ 백준/Bronze/2739. 구구단/구구단.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        if(1 < n && n <= 9) {
+            for (int i = 1; i < 10; i++) {
+                System.out.printf("%d * %d = %d\n", n, i, n * i);
+            }
+        }
+
+
+    }
+}
