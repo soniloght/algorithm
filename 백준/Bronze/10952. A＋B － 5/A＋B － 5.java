2075067fb98fe46186742e38607a14515d28e636
Hyeonsu Son <101486154+soniloght@users.noreply.github.com>
2025-03-04 20:43:41 +0900

    [Bronze V] Title: A+B - 5, Time: 68 ms, Memory: 11692 KB -BaekjoonHub

---
+++ 백준/Bronze/10952. A＋B － 5/A＋B － 5.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+
+        while (true){
+            st = new StringTokenizer(br.readLine());
+
+            int A = Integer.parseInt(st.nextToken());
+            int B = Integer.parseInt(st.nextToken());
+
+            if(A == 0 || B == 0){
+                break;
+            }
+            bw.write(Integer.toString(A+B) + "\n");
+        }
+        bw.close();
+    }
+}
